c3768a792592f798d170edb773aff13021a2541a
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-06 15:27:53 +0100

    Play redirect

--- app.rb
+++ app.rb
@@ -8,14 +8,22 @@ class Battle < Sinatra::Base
 
   # our routes would go here
 
+  enable :sessions
+
   get '/' do 
     "Testing infrastructure working!"
     erb :index 
   end 
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
