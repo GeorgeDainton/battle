072c4dd1639224c696ec6e932d638542c2a3b382
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-06 16:19:56 +0100

    refactored test with web helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,13 +5,7 @@ Capybara.app = Battle
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: "George"
-    fill_in :player_2_name, with: "Russell"
-    click_button 'Submit'
-
-    save_and_open_page
-    
+    sign_in_and_play
     expect(page).to have_content "George vs. Russell"
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -5,13 +5,7 @@ Capybara.app = Battle
 
 feature 'Hit points' do
   scenario 'it shows player hit points' do 
-    visit('/')
-    fill_in :player_1_name, with: "George"
-    fill_in :player_2_name, with: "Russell"
-    click_button 'Submit'
-
-    save_and_open_page
-
+    sign_in_and_play
     expect(page).to have_content "George HP: 100"
   end
 end 
